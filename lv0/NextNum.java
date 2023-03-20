05fbcc80a596136a9a0aab2ca73de73e1033dee6
KyungHyunHan <hantim4@gmail.com>
2023-03-20 23:14:00 +0900

    lv0 add Question

--- lv0/NextNum.java
+++ lv0/NextNum.java
@@ -1,5 +1,9 @@
 package lv0;
-
+/*
+ * 등차수열 혹은 등비수열 common이 매개변수로 주어질 때,
+ * 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+ * 
+ * */
 public class NextNum {
 
 	public int solution(int[] common) {   
