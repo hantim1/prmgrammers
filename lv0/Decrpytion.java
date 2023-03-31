48902406d7a943804a5c3e3de28ce941aa6a94e1
KyungHyunHan <hantim4@gmail.com>
2023-03-31 21:51:26 +0900

    Decrpytion

--- lv0/Decrpytion.java
+++ lv0/Decrpytion.java
@@ -1,5 +1,15 @@
 package lv0;
 
 public class Decrpytion {
+	public String solution(String cipher, int code) {
+		String answer = "";
+		char[] arr = new char[(cipher.length() / code)+1];
 
+		for (int i = 1; i< arr.length; i++) {
+			arr[i] = cipher.charAt(code * i - 1);
+			answer += arr[i];
+		}
+
+		return answer;
+	}
 }
